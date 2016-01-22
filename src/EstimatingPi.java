c14348abcf81bb84cff07a9672c3275868d15a1d
Jeremy Evert <jeremy.evert@swosu.edu>
2016-01-22 10:50:33 -0600

    Good enough for end of class.

--- src/EstimatingPi.java
+++ src/EstimatingPi.java
@@ -1,6 +1,13 @@
 public class EstimatingPi {
 	public static void main(String[] args) {
 		System.out.println("This is an estimate of pi.");
+		System.out.println("First Estimate");
+		System.out.println(4.0);
+		System.out.println("Second Estimate");
+		System.out.println(4.0 * (1.0 -(1.0/3)));
+		System.out.println("Third Estimate");
+		System.out.println(4.0 * (1.0 -(1.0/3)+(1.0/5)));
+		
 
 	}
 
